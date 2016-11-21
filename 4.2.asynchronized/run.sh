808f29325af508d65eff0aa42160a81f2fca5e5a
Wang Kai <kaige9at@hotmail.com>
2016-11-21 18:14:53 +0800

    修改测试

--- 4.2.asynchronized/run.sh
+++ 4.2.asynchronized/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mvn clean install -Dmaven.test.skip=true
-java -jar -Xms2048m -Xmx2048m -XX:NewSize=256m -XX:MaxNewSize=256m target/asynchronized-0.0.1-SNAPSHOT.jar
+java -jar -Xms512m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -Dfile.encoding=UTF-8 target/asynchronized-0.0.1-SNAPSHOT.jar
